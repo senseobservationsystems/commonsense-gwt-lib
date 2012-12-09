30ddd7097055a6e1512a679f3b0d14094d52b604
Steven Mulder <steven@sense-os.nl>
2012-12-09 16:36:00 +0100

    Added setter methods to group model

--- src/nl/sense_os/commonsense/lib/client/model/apiclass/Group.java
+++ src/nl/sense_os/commonsense/lib/client/model/apiclass/Group.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayString;
 
 /**
  * Overlay for group objects that are sent from CommonSense back end using JSON.
@@ -134,5 +135,101 @@ public class Group extends JavaScriptObject {
 
     public native final boolean isAllowDeleteSensors() /*-{
 		return this.default_remove_sensors == true;
+    }-*/;
+
+    public native final void setAccessPassword(String s) /*-{
+		this.access_password = s;
+    }-*/;
+
+    public native final void setAllowCreateSensors(boolean b) /*-{
+		this.default_add_sensors = b;
+    }-*/;
+
+    public native final void setAllowCreateUsers(boolean b) /*-{
+		this.default_add_users = b;
+    }-*/;
+
+    public native final void setAllowDeleteSensors(boolean b) /*-{
+		this.default_remove_sensors = b;
+    }-*/;
+
+    public native final void setAllowDeleteUsers(boolean b) /*-{
+		this.default_remove_users = b;
+    }-*/;
+
+    public native final void setAllowReadSensors(boolean b) /*-{
+		this.default_list_sensors = b;
+    }-*/;
+
+    public native final void setAllowReadUsers(boolean b) /*-{
+		this.default_list_users = b;
+    }-*/;
+
+    public native final void setAnonymous(boolean b) /*-{
+		this.anonymous = b;
+    }-*/;
+
+    public native final void setDescription(String s) /*-{
+		this.description = s;
+    }-*/;
+
+    public native final void setEmail(String s) /*-{
+		this.email = s;
+    }-*/;
+
+    public native final void setHidden(boolean b) /*-{
+		this.hidden = b;
+    }-*/;
+
+    public native final void setId(String s) /*-{
+		this.id = s;
+    }-*/;
+
+    public native final void setName(String s) /*-{
+		this.name = s;
+    }-*/;
+
+    public native final void setOptSensors(JsArrayString array) /*-{
+		this.optional_sensors = array;
+    }-*/;
+
+    public native final void setPassword(String s) /*-{
+		this.password = s;
+    }-*/;
+
+    public native final void setPublic(boolean b) /*-{
+		this['public'] = b;
+    }-*/;
+
+    public native final void setReqSensors(JsArrayString array) /*-{
+		this.required_sensors = array;
+    }-*/;
+
+    public native final void setShowEmailReq(boolean b) /*-{
+		this.required_show_email = b;
+    }-*/;
+
+    public native final void setShowFirstNameReq(boolean b) /*-{
+		this.required_show_first_name = b;
+    }-*/;
+
+    public native final void setShowIdReq(boolean b) /*-{
+		this.required_show_id = b;
+    }-*/;
+
+    public native final void setShowPhoneReq(boolean b) /*-{
+		this.required_show_phone_number = b;
+    }-*/;
+
+    public native final void setShowSurnameReq(boolean b) /*-{
+		this.required_show_surname = b;
+    }-*/;
+
+    public native final void setShowUsernameReq(boolean b) /*-{
+		this.required_show_username = b;
+    }-*/;
+
+    public native final void setUsername(String s) /*-{
+		this.username = s;
     }-*/;
 }
